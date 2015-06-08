b127864cabac4bc142ea375671428571296b811c
Tianshu Ren <tianshuren@Tianshus-MacBook-Pro.local>
2015-06-08 21:36:26 +0800

    added solution to problem 3 and 4

---
+++ problem3.py
@@ -0,0 +1,30 @@
+"""
+Solution to the following problem:
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+
+https://projecteuler.net/problem=2
+
+"""
+
+from numpy import sqrt
+
+n = 600851475143
+lpf = 1
+
+if n % 2 == 0:
+	lpf = 2
+	while n % 2 == 0:
+		n = n / 2
+
+k = 3
+ulimit = sqrt(n)
+while n != 1 and k < ulimit:
+	if n % k == 0:
+		n = n / k
+		lpf = k
+	else:
+		k = k + 2
+
---
+++ problem4.py
@@ -0,0 +1,28 @@
+"""
+Solution to the following problem:
+
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 * 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+
+https://projecteuler.net/problem=2
+
+"""
+
+def isPalindrome(n):
+	s = str(n)
+	sr = s[::-1]
+	return s == sr
+
+i1 = 999
+lp = 0
+
+while i1 > 100:
+	i2 = 999
+	while i2 > 100:
+		if i1 * i2 < lp:
+			break
+		if isPalindrome(i1 * i2):
+			lp = i1 * i2
+		i2 = i2 - 1
+	i1 = i1 - 1
