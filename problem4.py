4c4e8bc903cf6a0d525f2cce85537d7572335f84
Tianshu Ren <tianshuren@Tianshus-MacBook-Pro.local>
2015-06-09 16:33:44 +0800

    corrected url in description

--- problem4.py
+++ problem4.py
@@ -5,7 +5,7 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 
 Find the largest palindrome made from the product of two 3-digit numbers.
 
-https://projecteuler.net/problem=2
+https://projecteuler.net/problem=4
 
 """
 
